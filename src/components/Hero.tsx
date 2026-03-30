df9a47d3395f4d18fcbaa64e2312bb67a4beddc4
Emmilija <karatashevskaemilija@gmail.com>
2026-03-30 21:41:39 +0200

    added button for maps

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
 
 const Hero: React.FC = () => {
+
   return (
 <section className="relative w-full h-screen">
 
@@ -13,10 +14,18 @@ const Hero: React.FC = () => {
 
   <div className="absolute inset-0 bg-black/50"></div>
 
-  <div className="relative z-10 flex items-center justify-center h-full text-center text-white">
+  <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white">
     <h1 className="text-4xl md:text-6xl font-bold">
       Villa Paradise
     </h1>
+   <button
+          onClick={() =>
+            window.open(
+              "https://www.google.com/maps/search/Akti+Koviou+1,+Sithonia+630+88,+Greece/@40.1826677,23.6930904,14z",
+              "_blank"
+            )
+          }
+          className="underline mt-2">Akti Koviou</button>
   </div>
 
 </section>
